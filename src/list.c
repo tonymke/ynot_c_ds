d04de4e7b94f7b1d91acc5fed42666673800d398
Tony Lechner <tony@tony-lechner.com>
2022-05-24 00:01:52 -0500

    Wipe existing list implementation

--- src/list.c
+++ src/list.c
@@ -1,203 +1,31 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "ynot_c_ds.h"
 
-struct list* list_alloc()
+struct list {
+	int a;
+	int b;
+};
+
+list *foo(int a, int b)
 {
-	struct list *l;
-
-	l = malloc(sizeof(struct list));
-	l->next = NULL;
-	l->prev = NULL;
-	l->data = NULL;
-	return l;
-}
-
-struct list *list_append(struct list *l, void *data)
-{
-	l = list_end(l);
-
-	if (l == NULL) {
-		l = list_alloc();
-		l->data = data;
-		return l;
-	}
-
-	l->next = list_alloc();
-	l->next->prev = l;
-	l->next->data = data;
-	return l->next;
-}
-
-struct list *list_delete(struct list *l)
-{
-	struct list *remaining;
-	if (l == NULL)
+	list *l;
+	if ((l = malloc(sizeof(list))) == NULL) {
+		perror("foo: malloc");
 		return NULL;
-
-	remaining = NULL;
-
-	if (l->next != NULL) {
-		remaining = l->next;
-		l->next->prev = l->prev;
 	}
 
-	if (l->prev != NULL) {
-		remaining = l->prev;
-		l->prev->next = l->next;
-	}
-
-	free(l);
-
-	return remaining;
-}
-
-struct list *list_end(struct list *l)
-{
-	if (l == NULL)
-		return NULL;
-
-	while (l->next != NULL)
-		l = l->next;
+	l->a = a;
+	l->b = b;
 
 	return l;
 }
 
-void list_free_full(struct list *l, void (*data_free)(void *data))
+int bar(list *l)
 {
-	struct list *buf, *left_entry;
-	if (l == NULL)
-		return;
-
-	left_entry = l->prev;
-	while (l != NULL) {
-		if (data_free != NULL)
-			data_free(l->data);
-
-		buf = l;
-		l = l->next;
-		free(buf);
-	}
-
-	l = left_entry;
-	while (l != NULL) {
-		if (data_free != NULL)
-			data_free(l->data);
-		buf = l;
-		l = l->prev;
-		free(buf);
-	}
-}
-
-struct list *list_get(struct list *l, size_t i)
-{
-	size_t j;
-
-	for (j = 0, l = list_start(l); j < i && l != NULL; j++, l = l->next) {
-	}
-
-	return i == j ? l : NULL;
-}
-
-size_t list_len(struct list *l)
-{
-	size_t n;
-	struct list *left_buf;
 	if (l == NULL)
 		return 0;
 
-	l = list_start(l);
-	left_buf = l->prev;
-	for (n = 0; l != NULL; n++, l = l->next) {
-	}
-	for(l = left_buf; l != NULL; n++, l = l->prev) {
-	}
-
-	return n;
-}
-
-struct list *list_insert(struct list *l, void *data, size_t i)
-{
-	size_t j;
-	struct list *new;
-
-	new = list_alloc();
-	new->data = data;
-
-	j = 0;
-	while (l != NULL && j < i) {
-		new->prev = l;
-		new->next = l = l->next;
-		j++;
-	}
-
-	if (new->next != NULL)
-		new->next->prev = new;
-	if (new->prev != NULL)
-		new->prev->next = new;
-
-	return new;
-}
-
-void *list_pop(struct list *l)
-{
-	void *data;
-
-	if (l == NULL)
-		return NULL;
-
-	l = list_end(l);
-	data = l->data;
-
-	if (l->prev != NULL)
-		l->prev->next = NULL;
-
-	free(l);
-
-	return data;
-}
-
-void *list_pop_left(struct list *l)
-{
-	void *data;
-
-	if (l == NULL)
-		return NULL;
-
-	l = list_start(l);
-	data = l->data;
-
-	if (l->next != NULL)
-		l->next->prev = NULL;
-
-	free(l);
-
-	return data;
-}
-
-struct list *list_prepend(struct list *l, void *data)
-{
-	l = list_start(l);
-	if (l == NULL) {
-		l = list_alloc();
-		l->data = data;
-		return l;
-	}
-
-	l->prev = list_alloc();
-	l->prev->next = l;
-	l->prev->data = data;
-
-	return l->prev;
-}
-
-struct list *list_start(struct list *l)
-{
-	if (l == NULL)
-		return NULL;
-
-	while (l->prev != NULL)
-		l = l->prev;
-
-	return l;
+	return l->a + l->b;
 }
--- src/ynot_c_ds.h
+++ src/ynot_c_ds.h
@@ -3,28 +3,10 @@
 #ifndef YNOT_C_DS_H
 #define YNOT_C_DS_H
 
-struct list {
-	struct list *next;
-	struct list *prev;
+struct list;
+typedef struct list list;
 
-	void *data;
-};
-
-struct list* list_alloc();
-#define list_free(l) (list_free_full((l), NULL))
-void list_free_full(struct list *l, void (*data_free)(void *data));
-
-struct list *list_end(struct list *l);
-struct list *list_get(struct list *l, size_t i);
-size_t list_len(struct list *l);
-struct list *list_start(struct list *l);
-
-struct list *list_append(struct list *l, void *data);
-struct list *list_insert(struct list *l, void *data, size_t i);
-struct list *list_prepend(struct list *l, void *data);
-
-struct list *list_delete(struct list *l);
-void *list_pop(struct list *l);
-void *list_pop_left(struct list *l);
+list *foo(int a, int b);
+int bar(list *l);
 
 #endif /* YNOT_C_DS_H */
