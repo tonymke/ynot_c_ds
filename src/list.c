983ef4c61d8e7708187c75bcbbf875be56d01c17
Tony Lechner <tony@tony-lechner.com>
2022-05-24 01:22:45 -0500

    Define new list API

--- src/list.c
+++ src/list.c
@@ -2,30 +2,3 @@
 #include <stdlib.h>
 
 #include "ynot_c_ds.h"
-
-struct list {
-	int a;
-	int b;
-};
-
-list *foo(int a, int b)
-{
-	list *l;
-	if ((l = malloc(sizeof(list))) == NULL) {
-		perror("foo: malloc");
-		return NULL;
-	}
-
-	l->a = a;
-	l->b = b;
-
-	return l;
-}
-
-int bar(list *l)
-{
-	if (l == NULL)
-		return 0;
-
-	return l->a + l->b;
-}
--- src/ynot_c_ds.h
+++ src/ynot_c_ds.h
@@ -4,9 +4,20 @@
 #define YNOT_C_DS_H
 
 struct list;
+
 typedef struct list list;
 
-list *foo(int a, int b);
-int bar(list *l);
+list  *list_alloc    (void);
+int    list_append   (list *lst, void *value);
+void   list_free_full(list *lst, void (*free_value)(void *value));
+void  *list_get      (list *lst, size_t i);
+int    list_insert   (list *lst, void *value, size_t i);
+size_t list_len      (list *lst);
+void  *list_pop      (list *lst);
+void  *list_pop_left (list *lst);
+int    list_prepend  (list *lst, void *value);
+void  *list_remove   (list *lst, size_t i);
+
+#define list_free(l) (list_free_full((l), NULL))
 
 #endif /* YNOT_C_DS_H */
