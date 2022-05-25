fa2880b52b712e4a0529a659f046a06a20f25dbc
Tony Lechner <tony@tony-lechner.com>
2022-05-24 20:07:31 -0500

    Update array prototypes

--- src/ynot_c_ds.h
+++ src/ynot_c_ds.h
@@ -3,12 +3,13 @@
 
 #include <stdlib.h>
 
-enum ynot_return_status {
-	YNOT_OK,
+enum ynot_errors {
+	YNOT_OK = 0,
 
-	YNOT_EINVALIDPARAM,
-	YNOT_ENOMEM,
-	YNOT_EOUTOFRANGE
+	YNOT_EUNKNOWN      = -1,
+	YNOT_EINVALIDPARAM = -2,
+	YNOT_ENOMEM        = -3,
+	YNOT_EOUTOFRANGE   = -4
 };
 
 struct array;
@@ -17,19 +18,17 @@ struct list;
 typedef struct array array;
 typedef struct list list;
 
-array *array_alloc_capacity (size_t initial_capacity);
-int    array_append         (array *arr, void *value);
-void   array_free_full      (array *arr, void (*free_value)(void *value));
-int    array_insert         (array *arr, void *value, size_t i);
-size_t array_len            (array *arr);
-void  *array_peek           (array *arr);
-void  *array_peek_at        (array *arr, size_t i);
-void  *array_peek_left      (array *arr);
-void  *array_pop            (array *arr);
-void  *array_pop_left       (array *arr);
-int    array_prepend        (array *arr, void *value);
-void  *array_remove_at      (array *arr, size_t i);
-void  *array_set            (array *arr, void *value, size_t i);
+int    array_add             (array *arr, void *value);
+array *array_alloc_capacity  (size_t initial_capacity);
+void   array_clear           (array *arr);
+void   array_free_full       (array *arr, void (*free_value)(void *value));
+int    array_insert          (array *arr, size_t i, void *value);
+int    array_ensure_capacity (array *arr, size_t capacity);
+size_t array_len             (array *arr);
+void  *array_get             (array *arr, size_t i);
+void  *array_remove_at       (array *arr, size_t i);
+void  *array_set             (array *arr, size_t i, void *value);
+int    array_trim            (array *arr);
 
 list  *list_alloc     (void);
 int    list_append    (list *lst, void *value);
@@ -44,9 +43,8 @@ void  *list_pop_left  (list *lst);
 int    list_prepend   (list *lst, void *value);
 void  *list_remove_at (list *lst, size_t i);
 
-#define array_alloc()   (array_alloc_capacity(0))
-#define array_get(A, I) (array_peek_at((A) (I))
-#define array_free(A)   (array_free_full((A), NULL))
-#define list_free(L)    (list_free_full((L), NULL))
+#define array_alloc()       (array_alloc_capacity(0))
+#define array_free(A)       (array_free_full((A), NULL))
+#define list_free(L)        (list_free_full((L), NULL))
 
 #endif /* YNOT_C_DS_H */
