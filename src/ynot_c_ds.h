c410751831b6878f3522241184a8dccf5b630718
Tony Lechner <tony@tony-lechner.com>
2022-05-26 09:06:56 -0500

    Sort public header prototypes

--- src/ynot_c_ds.h
+++ src/ynot_c_ds.h
@@ -20,29 +20,29 @@ typedef struct list list;
 typedef struct map map;
 
 
-int    array_add             (array *arr, void *value);
 array *array_alloc_capacity  (size_t initial_capacity);
+int    array_add             (array *arr, void *value);
+int    array_ensure_capacity (array *arr, size_t min_capacity);
+int    array_insert          (array *arr, size_t i, void *value);
+int    array_trim_capacity   (array *arr);
+size_t array_len             (array *arr);
 void   array_clear           (array *arr);
 void   array_free_full       (array *arr, void (*free_value)(void *value));
-int    array_insert          (array *arr, size_t i, void *value);
-int    array_ensure_capacity (array *arr, size_t min_capacity);
-size_t array_len             (array *arr);
 void  *array_get             (array *arr, size_t i);
 void  *array_remove_at       (array *arr, size_t i);
 void  *array_set             (array *arr, size_t i, void *value);
-int    array_trim_capacity   (array *arr);
 
-list  *list_alloc     (void);
 int    list_append    (list *lst, void *value);
-void   list_free_full (list *lst, void (*free_value)(void *value));
 int    list_insert    (list *lst, size_t i, void *value);
+int    list_prepend   (list *lst, void *value);
+list  *list_alloc     (void);
 size_t list_len       (list *lst);
+void   list_free_full (list *lst, void (*free_value)(void *value));
 void  *list_peek      (list *lst);
 void  *list_peek_at   (list *lst, size_t i);
 void  *list_peek_left (list *lst);
 void  *list_pop       (list *lst);
 void  *list_pop_left  (list *lst);
-int    list_prepend   (list *lst, void *value);
 void  *list_remove_at (list *lst, size_t i);
 
 map *map_alloc_capacity    (size_t (*hash_fn)(void* key),
